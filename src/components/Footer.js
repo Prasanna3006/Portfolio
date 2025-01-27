7040982f79d86c57044c4f186a6b1f99e7a565a8
Your Name <you@example.com>
2025-01-27 18:23:13 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 
 export const Footer = () => {
@@ -17,19 +16,13 @@ export const Footer = () => {
   <div className="contact-item">
     <i className="fas fa-phone-alt"></i>
     <p>Phone: <a href="tel:+918610362554">+91 8610362554</a></p>
-  </div>
-  <div className="contact-item">
-    <i className="fas fa-map-marker-alt"></i>
-    <p>Address: Salem, Tamilnadu, India</p>
-  </div>
+  </div><div className="social-icon">
+    <a href="https://www.linkedin.com/in/prasanna-m-a09bb024a/"><img src={navIcon1} alt="Icon" /></a>
+    </div>
 </div>
-
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/prasanna-m-a09bb024a/"><img src={navIcon1} alt="Icon" /></a>
               <p>Copyright 2025. All Rights Reserved</p>
-            </div>
           </Col>
         </Row>
       </Container>
