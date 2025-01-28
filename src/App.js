bbb66598098b8ce22d4d13779ed1c378396002f9
Your Name <you@example.com>
2025-01-28 17:46:20 +0530

    button change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
+
 function App() {
   return (
+    
     <div className="App">
       <NavBar />
       <Banner />
